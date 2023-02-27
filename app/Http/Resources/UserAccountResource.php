f78c91f5c423dc491ce886a44e00beccdef290e6
Ikhlasul Amal <54785909+ikhlasul0507@users.noreply.github.com>
2023-02-27 15:29:31 +0700

    change version

--- app/Http/Resources/UserAccountResource.php
+++ app/Http/Resources/UserAccountResource.php
@@ -15,7 +15,7 @@ class UserAccountResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'version'   => '1.1.1',
+            'version'   => '1.0.0',
             'serverAddress'   => '11.11.11.11',
             'statusSuccess'   => $this->status,
             'message'   => $this->message,
