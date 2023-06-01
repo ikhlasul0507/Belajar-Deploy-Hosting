6966bf8e50f108c932d262e815e9d30995395448
Ikhlasul Amal <54785909+ikhlasul0507@users.noreply.github.com>
2023-06-02 06:42:28 +0700

    updated

--- page-login/login.php
+++ page-login/login.php
@@ -2,18 +2,17 @@
 <html lang="en">
 
 <head class="lib-login-head"></head>
-<?php 
-    $root = (isset($_SERVER['HTTPS']) ? "https://" : "http://") . $_SERVER['HTTP_HOST']. '/';    
-    $root .= explode("/",str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']))[1];
+<?php
+$root = (isset($_SERVER['HTTPS']) ? "https://" : "http://") . $_SERVER['HTTP_HOST'];
+// $root .= explode("/", str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']))[1];
 ?>
+
 <body>
     <div class="col-lg-12">
         <div class="row" style="height: 100vh">
             <div class="col-lg-8 d-none d-lg-block bg-gradient-danger nav-login"></div>
             <div class="col-lg-4  bg-gradient-light ">
-                <span class="badge rounded-pill bg-success"><i class="fas fa-check text-light"></i></span> <span
-                    class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span><small
-                    class="resultPing">....</small>
+                <span class="badge rounded-pill bg-success"><i class="fas fa-check text-light"></i></span> <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span><small class="resultPing">....</small>
                 <div class="p-2 mt-2">
                     <div class="text-center">
                         <h1 class="h4 text-gray-900 mb-4 label-login">....</h1>
@@ -32,10 +31,10 @@
             </div>
         </div>
     </div>
-    <script src="<?= $root;?>/vendor/jquery/jquery.min.js"></script>
-    <script src="<?= $root;?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="<?= $root;?>/vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="<?= $root;?>/js/sb-admin-2.min.js"></script>
+    <script src="<?= $root; ?>/vendor/jquery/jquery.min.js"></script>
+    <script src="<?= $root; ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= $root; ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?= $root; ?>/js/sb-admin-2.min.js"></script>
     <script type="module" src="page-login/login.js"></script>
     <script type="module" src="page-login/config.js"></script>
 </body>
